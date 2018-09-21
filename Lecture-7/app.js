7a03a167830ac813f0cc188af0876c15508d4f70
aayusharora <aayush113002@gmail.com>
2018-09-21 15:08:00 +0530

    Added Lecture-7

---
+++ Lecture-7/app.js
@@ -0,0 +1,22 @@
+var x = 'apple';
+console.log(x);
+
+// Hoisting 
+
+
+
+// Declaring a Function Expression
+var animal = function() {
+	var nam = 'cat';
+	return nam;
+}
+
+
+// Declaring a function
+function greet() {
+	var username = 'ravi';
+	return "main1";
+}
+
+greet();
+console.log(username);
